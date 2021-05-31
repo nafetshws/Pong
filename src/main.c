6bb47adf49b9b9c30c233a0a91b43b846d8b9bc7
nafetshws <stefan0507hws21@gmail.com>
2021-05-31 21:27:47 +0200

    added triangle

--- src/main.c
+++ src/main.c
@@ -4,6 +4,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+void error_callback(int error, const char* description);
+void user_close_callback(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+static void key_callback(GLFWwindow* window, int key, int scancdoe, int action, int mods);
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main(){\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main(){\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+int main(){  
+
+
+  glfwSetErrorCallback(error_callback);
+  if(!glfwInit()){
+    printf("Failed to initialize\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Successfully initialized\n");
+
+  GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+  //version that every graphics driver supports
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //set core profile
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  //create window
+  GLFWwindow* window = glfwCreateWindow(680, 480, "Pong", NULL, NULL);
+
+  if(!window){
+    printf("Failed to create window :(\n");
+    glfwTerminate();
+    return -1;
+  }
+  //set context
+  glfwMakeContextCurrent(window);
+  
+  //extensio loader libary
+  int result = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  if(!result){
+    printf("Failed to init GLAD\n");
+    return -1;
+  }
+  else{
+    printf("successfully loaded glad\n");
+  }
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.f,
+    0.5f, -0.5f, 0.f,
+    0.f, 0.5f, 0.f,
+  };
+
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if(!success){
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("shader vertex compilation failed: %s\n", infoLog);
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if(!success){
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    printf("shader vertex compilation failed: %s\n", infoLog);
+  }
+  
+
+  //shader program
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if(!success){
+    printf("Failed to link shaders\n");
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+  }
+
+  //don't need the shaders anymore -> got the shaderProgram
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+
+  //events
+  //input
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetCursorPosCallback(window, cursor_position_callback);
+
+  glfwSetWindowCloseCallback(window, user_close_callback);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while(!glfwWindowShouldClose(window)){
+    //window is open
+    glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  //clear memory
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
+
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
   //printf("Mouse was moved: x: %f y: %f\n", xpos, ypos);
@@ -20,6 +165,7 @@ void user_close_callback(GLFWwindow* window){
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
   glViewport(0, 0, width, height);
 }
+
 
 static void key_callback(GLFWwindow* window, int key, int scancdoe, int action, int mods){
   //printf("key pressed: %d\n", key);
@@ -39,90 +185,4 @@ static void key_callback(GLFWwindow* window, int key, int scancdoe, int action, 
     height = 1200;
     glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), xpos, ypos, width, height, GLFW_DONT_CARE);
     }
-}
-
-int main(){  
-
-  glfwSetErrorCallback(error_callback);
-  if(!glfwInit()){
-    printf("Failed to initialize\n");
-    exit(EXIT_FAILURE);
-  }
-  printf("Successfully initialized\n");
-
-  GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-  //version that every graphics driver supports
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  //set core profile
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //create window
-  GLFWwindow* window = glfwCreateWindow(680, 480, "Pong", NULL, NULL);
-
-  if(!window){
-    printf("Failed to create window :(");
-    glfwTerminate();
-    return -1;
-  }
-  //set context
-  glfwMakeContextCurrent(window);
-  
-  //extensio loader libary
-  int result = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-  if(!result){
-    printf("Failed to init GLAD\n");
-    return -1;
-  }
-  else{
-    printf("successfully loaded glad\n");
-  }
-
-  //events
-  //input
-  glfwSetKeyCallback(window, key_callback);
-  glfwSetCursorPosCallback(window, cursor_position_callback);
-
-  glfwSetWindowCloseCallback(window, user_close_callback);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  while(!glfwWindowShouldClose(window)){
-    //window is open
-    glClearColor(0.2f, 0.3f, 0.3f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    //float ratio;
-    //int width, height;  
-    //glfwGetFramebufferSize(window, &width, &height);
-    //ratio = width / (float) height;
-    //glClear(GL_COLOR_BUFFER_BIT);
-
-    //glMatrixMode(GL_PROJECTION);
-    //glLoadIdentity();
-    //glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-    //
-    //glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();
-    //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-    //glTranslatef(-0.5, -0.5, 0);
-
-    //glBegin(GL_POLYGON);
-
-    ////sqaure
-    //glColor3f(1.f, 1.f, 1.f);
-    //glVertex2f(0.f, 0.f);
-    //glColor3f(1.f, 0.f, 0.f);
-    //glVertex2f(0.5f, 0.f);
-    //glColor3f(0.f, 1.f, 0.f);
-    //glVertex2f(0.5f, 0.5f);
-    //glColor3f(0.f, 0.f, 1.f);
-    //glVertex2f(0.f, 0.5f);
-
-    //glEnd();
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-  //clear memory
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
 }
