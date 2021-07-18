5db5d59a19605ed5747c135826d5e7886e87b47d
nafetshws <stefan0507hws21@gmail.com>
2021-07-18 20:33:03 +0200

    raw unchecked code -> backup

--- include/type_structs.h
+++ include/type_structs.h
@@ -6,6 +6,7 @@
 
 enum CollisionType {COLLISION_TOP, COLLISION_BOTTOM, COLLISION_LEFT_PADDLE, COLLISION_RIGHT_PADDLE, COLLISION_LEFT_WALL, COLLISION_RIGHT_WALL, COLLISION_NONE};
 const char* CollisionTypeNames[7] = {"TOP", "BOTTOM", "LEFT_PADDLE", "RIGHT_PADDLE", "LEFT_WALL", "RIGHT_WALL", "COLLISION_NONE"};
+const int MAX_CAPACITY = 30;
 
 struct Ball{
   vec3 position;
@@ -40,11 +41,43 @@ struct map{
   struct Character character;
 };
 
-struct ListMap;
+struct ListMap{
+  struct map map[MAX_CAPACITY];
+  size_t count;
+}
 
-bool listMapInsert(struct ListMap* map, char key, struct Character character);
-struct Character listMapGetValue(struct ListMap* map, char key);
-struct ListMap* newListMap();
-void freeListMap(struct ListMap* listMap);
+struct ListMap* newListMap(){
+  struct ListMap* ret = calloc(1, sizeof(struct ListMap));
+  ret->count = 0;
+  return ret;
+}
+int listMapInsert(struct ListMap* listMap, char key, struct Character character){
+  if(listMap->count == MAX_CAPACITY){
+    return 1;
+  }
+  listMap->map[listMap->count].key = key;
+  listMap->map[listMap->count].character = character;
+  listMap->count++;
+  return 0;
+}
+struct Character listMapGetValue(struct ListMap* listMap, char key){
+  struct Character character = NULL;
+  for(size_t i = 0; i < listMap->count && character == NULL; i++){
+    if(listMap->map->key == key){
+      character = listMap->map->character;
+    }
+  }
+  return character;
+
+}
+void freeListMap(struct ListMap* listMap){
+  if(listMap == NULL){
+    return;
+  }
+  for(size_t i = 0; i < listMap->count; i++){
+    free(listMap->map[i]);
+  }
+  free(listMap);
+}
 
 #endif
